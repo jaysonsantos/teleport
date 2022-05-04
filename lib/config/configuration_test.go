5820fbe58db632d279d011269dda9187b3bf20b5
Noah <noah.stride@goteleport.com>
2022-05-04 14:37:38 +0100

    add configured paths to file permission errors

--- lib/config/configuration_test.go
+++ lib/config/configuration_test.go
@@ -2173,7 +2173,7 @@ func TestTLSCert(t *testing.T) {
 	}
 }
 
-func Test_applyKeyStoreConfig(t *testing.T) {
+func TestApplyKeyStoreConfig(t *testing.T) {
 	slotNumber := 1
 
 	tempDir := t.TempDir()
@@ -2267,7 +2267,10 @@ func Test_applyKeyStoreConfig(t *testing.T) {
 					},
 				},
 			},
-			wantErr: "PKCS11 library must not be world-writable",
+			wantErr: fmt.Sprintf(
+				"PKCS11 library (%s) must not be world-writable",
+				worldWritablePKCS11LibPath,
+			),
 		},
 		{
 			name: "err when pin file world-readable",
@@ -2278,7 +2281,10 @@ func Test_applyKeyStoreConfig(t *testing.T) {
 					},
 				},
 			},
-			wantErr: "HSM pin file must not be world-readable",
+			wantErr: fmt.Sprintf(
+				"HSM pin file (%s) must not be world-readable",
+				worldReadablePinFilePath,
+			),
 		},
 	}
 
